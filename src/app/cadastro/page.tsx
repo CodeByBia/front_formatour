6703f83c98e7c6dbbd814cc8af0313cf63dd9028
mellopes06br <melferreira1808@gmail.com>
2025-06-08 14:12:52 -0300

    feat: adicionando o email.

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -33,6 +33,18 @@ export default function CadastroPage() {
                 onChange={e => setNome(e.target.value)}
                 placeholder="Nome completo"
               />
+              </div>
+            <div className="flex flex-col gap-1">
+              <label className="text-[#3B5D46] text-sm font-semibold" htmlFor="email">Email</label>
+              <input
+                id="email"
+                className="rounded border border-gray-300 px-4 py-2 bg-white text-black focus:outline-none focus:ring-2 focus:ring-[#3B5D46]"
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Roberttoalves@gmail.com"
+              />
+            </div>
             
             <div className="flex flex-col gap-1">
               <label className="text-[#3B5D46] text-sm font-semibold" htmlFor="senha">Senha</label>
