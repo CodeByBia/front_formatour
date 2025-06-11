7bd499857379de7a13135290f43accd5a0cd4e50
Beariz martins <beatrizmartins3252@gmail.com>
2025-06-11 14:31:44 -0300

    Revert "adicionado pasta para zustend"
    This reverts commit a78d1b9ea593651fb7019f4ba84c5adde26e2cc4.
    
    consertando erro de exclusão de codigo

--- src/store/useLoadingStore.ts
+++ src/store/useLoadingStore.ts
@@ -1,1 +1,11 @@
-//@mel adicionar
+import { create } from 'zustand';
+
+interface LoadingState {
+  isLoading: boolean;
+  setLoading: (loading: boolean) => void;
+}
+
+export const useLoadingStore = create<LoadingState>((set) => ({
+  isLoading: false,
+  setLoading: (loading) => set({ isLoading: loading }),
+}));
