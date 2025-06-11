a78d1b9ea593651fb7019f4ba84c5adde26e2cc4
Beariz martins <beatrizmartins3252@gmail.com>
2025-06-11 00:15:07 -0300

    adicionado pasta para zustend

--- src/store/useLoadingStore.ts
+++ src/store/useLoadingStore.ts
@@ -1,11 +1,1 @@
-import { create } from 'zustand';
-
-interface LoadingState {
-  isLoading: boolean;
-  setLoading: (loading: boolean) => void;
-}
-
-export const useLoadingStore = create<LoadingState>((set) => ({
-  isLoading: false,
-  setLoading: (loading) => set({ isLoading: loading }),
-}));
+//@mel adicionar
